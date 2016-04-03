986140db644b5bb8e87cb9b83402bc4b781220db
Ryan C. Gordon <icculus@icculus.org>
2016-04-03 00:00:04 -0400

    Build on Mac OS X with reported number of CPU cores.

--- Sources/build-mac.sh
+++ Sources/build-mac.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+NCPU=`sysctl -n hw.ncpu`
+echo "Will build with 'make -j$NCPU' ... please edit this script if incorrect."
+
 set -e
 set -x
 
@@ -7,5 +10,5 @@ rm -rf cmake-build
 mkdir $_
 cd $_
 cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_OSX_ARCHITECTURES=i386 ..
-make -j8
+make -j$NCPU
 
