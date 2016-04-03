f9bfaf7435b8aa0438073afb9389f137bee25894
Ryan C. Gordon <icculus@icculus.org>
2016-04-03 00:07:19 -0400

    Initial add of build-linux.sh

---
+++ Sources/build-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NCPU=`cat /proc/cpuinfo |grep vendor_id |wc -l`
+let NCPU=$NCPU+2
+echo "Will build with 'make -j$NCPU' ... please edit this script if incorrect."
+
+set -e
+set -x
+
+rm -rf cmake-build
+mkdir $_
+cd $_
+#cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 ..
+#ninja
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 ..
+make -j$NCPU
+
