ec1448c4d46fce4615a1450208411d554097a8f6
Salamanderrake <salamanderrake@gmail.com>
2016-04-18 21:36:28 -0400

    build-linux32.sh: Renamed build-linux.sh build script to show which arch it is compiling build-linux64.sh: for use build native 64bit linux binaries.

---
+++ Sources/build-linux64.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+NCPU=`cat /proc/cpuinfo |grep vendor_id |wc -l`
+let NCPU=$NCPU+2
+echo "Will build with 'make -j$NCPU' ... please edit this script if incorrect."
+
+set -e
+set -x
+
+rm -rf cmake-build
+mkdir $_
+cd $_
+#cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 ..
+#ninja
+
+# This is the eventual path for amd64.
+cmake -DCMAKE_BUILD_TYPE=Debug -DUSE_I386_ASM=FALSE ..
+
+# Right now we force x86, though...
+#cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 ..
+
+make -j$NCPU
+
