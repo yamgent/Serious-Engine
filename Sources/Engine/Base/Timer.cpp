dbd896b58d287140920808c424c6a2a372a2cf46
Joshua Scoggins <Theoretically.x64@gmail.com>
2016-04-20 19:06:02 -0700

    Use the platform independent version of ReadTSC
    - If I use the gnu inline version, the game runs way too fast on my cpu due to
      frequency scaling (I think).

--- Sources/Engine/Base/Timer.cpp
+++ Sources/Engine/Base/Timer.cpp
@@ -29,7 +29,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include <Engine/Base/Priority.inl>
 
 // !!! FIXME: use SDL timer code instead and rdtsc never?
-#if (USE_PORTABLE_C)
+#if (USE_PORTABLE_C) || PLATFORM_UNIX
 #define USE_GETTIMEOFDAY 1
 #endif
 
